c1734b4ae83c0e4167f90c621d573685f9ffe7bc
hunterlarco <hunter@larcolabs.com>
2023-08-24 18:13:44 +0000

    ensure test is working

--- binaries/socket-client/src/index.ts
+++ binaries/socket-client/src/index.ts
@@ -1,19 +1,31 @@
+const commander = require('commander');
 const socketio = require('socket.io-client');
 
-const socket = socketio.io('ws://localhost:6080', { path: '/foo/connect' });
+commander.program
+  .name('socket-server client.')
+  .argument('<backend>', 'Backend to connect to.')
+  .action((backend) => {
+    console.log(`Attempting to connection to backend ${backend}.`);
 
-socket.on('connect', () => {
-  console.info('Connected!');
-});
+    const socket = socketio.io('ws://localhost:6080', {
+      path: `/${backend}/connect`,
+    });
 
-socket.on('connect_error', (error) => {
-  console.warn('Experienced connection error', error);
-});
+    socket.on('connect', () => {
+      console.info('Connected!');
+    });
 
-socket.on('connect_failed', (error) => {
-  console.warn('Failed to connect with error', error);
-});
+    socket.on('connect_error', (error) => {
+      console.warn('Experienced connection error', error);
+    });
 
-socket.on('disconnect', () => {
-  console.info('Disconnected');
-});
+    socket.on('connect_failed', (error) => {
+      console.warn('Failed to connect with error', error);
+    });
+
+    socket.on('disconnect', () => {
+      console.info('Disconnected');
+    });
+  });
+
+commander.program.parse();
